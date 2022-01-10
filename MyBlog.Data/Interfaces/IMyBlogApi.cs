52966ac3fb15a1a58eb49013aed1fabfe9c3bbba
yusheng07 <yusheng07@gmail.com>
2022-01-10 17:04:19 +0800

    0110v1

---
+++ MyBlog.Data/Interfaces/IMyBlogApi.cs
@@ -0,0 +1,30 @@
+﻿using MyBlog.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Interfaces
+{
+    public interface IMyBlogApi
+    {
+        Task<int> GetBlogPostCountAsync();
+        Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts,int startindex);
+        Task<List<Category>> GetCategoriesAsync();
+        Task<List<Tag>> GetTagsAsync();
+
+        Task<BlogPost> GetBlogPostAsync(int id);
+        Task<Category> GetCategoryAsync(int id);
+        Task<Tag> GetTagAsync(int id);
+
+        Task<BlogPost> SaveBlogPostAsync(BlogPost item);
+        Task<Category> SaveCategoryAsync(Category item);
+        Task<Tag> SaveTagAsync(Tag item);
+
+        Task DeleteBlogPostAsync(BlogPost item);
+        Task DeleteCategoryAsync(Category item);
+        Task DeleteTagAsync(Tag item);
+
+    }
+}
---
+++ MyBlog.Data/MyBlogApiServerSide.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Data.Interfaces;
+using MyBlog.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data
+{
+    internal class MyBlogApiServerSide : IMyBlogApi
+    {
+        IDbContextFactory<MyBlogDbContext> factory;
+        public MyBlogApiServerSide(IDbContextFactory<MyBlogDbContext> factory)
+        {
+            this.factory = factory;
+        }
+
+        #region GetBlogPost
+        public async Task<BlogPost> GetBlogPostAsync(int id)
+        {
+            using var context = factory.CreateDbContext();
+            return await context.BlogPosts.Include(p => p.Category)
+                                        .Include(p => p.Tags)
+                                        .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<int> GetBlogPostCountAsync()
+        {
+            using var context = factory.CreateDbContext();
+            return await context.BlogPosts.CountAsync();
+        }
+
+        public async Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts, int startindex)
+        {
+            using var context = factory.CreateDbContext();
+            return await context.BlogPosts.OrderByDescending(p => p.PublishDate)
+                                            .Skip(startindex)
+                                            .Take(numberofposts)
+                                            .ToListAsync();
+        } 
+        #endregion
+
+        #region GetCategory
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            using var context = factory.CreateDbContext();
+            return await context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryAsync(int id)
+        {
+            using var context = factory.CreateDbContext();
+            return await context.Categories.Include(p => p.BlogPosts)
+                                        .FirstOrDefaultAsync(c => c.Id == id);
+        } 
+        #endregion
+
+        #region GetTag
+        public async Task<Tag> GetTagAsync(int id)
+        {
+            using var context = factory.CreateDbContext();
+            return await context.Tags.Include(p => p.BlogPosts)
+                                    .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            using var context = factory.CreateDbContext();
+            return await context.Tags.ToListAsync();
+        }
+        #endregion
+
+        #region DeleteItem
+        private async Task DeleteItem(IMyBlogItem item)
+        {
+            using var context = factory.CreateDbContext();
+            context.Remove(item);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBlogPostAsync(BlogPost item)
+        {
+            await DeleteItem(item);
+        }
+
+        public async Task DeleteCategoryAsync(Category item)
+        {
+            await DeleteItem(item);
+        }
+
+        public async Task DeleteTagAsync(Tag item)
+        {
+            await DeleteItem(item);
+        }
+        #endregion
+
+        private async Task<IMyBlogItem> SaveItem(IMyBlogItem item)
+        {
+            using var context = factory.CreateDbContext();
+            if (item.Id == 0)
+            {
+                context.Add(item);
+            }
+            else
+            {
+                if (item is BlogPost)
+                {
+                    var post = item as BlogPost;
+                    var currentPost = await context.BlogPosts.Include(p => p.Category)
+                                                            .Include(p => p.Tags)
+                                                            .FirstOrDefaultAsync(p => p.Id == post.Id);
+                    currentPost.PublishDate = post.PublishDate;
+                    currentPost.Title = post.Title;
+                    currentPost.Text = post.Text;
+                    var post_tagIds = post.Tags.Select(t => t.Id);
+                    currentPost.Tags = context.Tags.Where(t => post_tagIds.Contains(t.Id)).ToList();
+                }
+                else
+                {
+                    context.Entry(item).State = EntityState.Modified;
+                }
+            }
+
+            context.Remove(item);
+            await context.SaveChangesAsync();
+        }
+
+
+
+
+
+        public Task<BlogPost> SaveBlogPostAsync(BlogPost item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> SaveCategoryAsync(Category item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> SaveTagAsync(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
